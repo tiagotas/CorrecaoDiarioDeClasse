91a81c38dd8e277a921291475e092e578aaebf30
TIAGO SILVA <frost.tiago@gmail.com>
2021-06-01 15:55:17 -0300

    Adicionar arquivos de projeto.

---
+++ CorrecaoDiarioDeClasse/Aluno.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrecaoDiarioDeClasse
+{
+    public class Aluno
+    {
+        int id;
+        string nome;
+        string data_nascimento;
+
+        public int Id
+        {
+            get => id;
+            set { id = value; }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public string DataNascimento
+        {
+            get => data_nascimento;
+            set { data_nascimento = value; }
+        }
+
+
+        public static void Listar_Alunos(List<Aluno> lista_alunos)
+        {
+            if (lista_alunos.Count == 0)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Nenhum aluno no Sistema");
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            else
+            {
+                for (int i = 0; i < lista_alunos.Count; i++)
+                {
+                    string aluno = string.Format(
+                        "Id: {0} - {1} - Nasc: {2}",
+                        lista_alunos[i].Id,
+                        lista_alunos[i].Nome,
+                        lista_alunos[i].DataNascimento
+                     );
+
+                    Console.WriteLine(aluno);
+                }
+            }
+        }
+
+        public static List<Aluno> Cadastrar(List<Aluno> lista_alunos)
+        {
+            Console.WriteLine("Qual é o nome do Aluno?");
+            string nome_tmp = Console.ReadLine();
+
+            Console.WriteLine("Qual é a Data de Nascimento do Aluno?");
+            string data_nascimento_tmp = Console.ReadLine();
+
+            lista_alunos.Add(new Aluno()
+            {
+                Id = lista_alunos.Count + 1,
+                Nome = nome_tmp,
+                DataNascimento = data_nascimento_tmp
+            });
+
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Aluno Inserido com Sucesso!");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            return lista_alunos;
+        }
+
+        public static List<Aluno> Atualizar(List<Aluno> lista_alunos)
+        {
+            Console.WriteLine("Qual é a ID do aluno a ser atualizado?");
+            int id_digitado = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < lista_alunos.Count; i++)
+            {
+                if(lista_alunos[i].Id == id_digitado)
+                {
+                    Console.WriteLine("Qual é o nome do Aluno? Nome atual: {0}", lista_alunos[i].Nome);
+                    lista_alunos[i].Nome = Console.ReadLine();
+
+                    Console.WriteLine("Qual é o Data de Nascimento do Aluno? Atual: {0}", lista_alunos[i].DataNascimento);
+                    lista_alunos[i].DataNascimento = Console.ReadLine();
+
+                    break;
+                }
+            }
+
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Aluno Atualizado com Sucesso!");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            return lista_alunos;
+        }
+
+        public static List<Aluno> Excluir(List<Aluno> lista_alunos)
+        {
+            Console.WriteLine("Qual é a ID do aluno a ser excluído?");
+            int id_digitado = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < lista_alunos.Count; i++)
+            {
+                if (lista_alunos[i].Id == id_digitado)
+                {
+                    lista_alunos.RemoveAt(i);
+                    break;
+                }
+            }
+
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Aluno Removido com Sucesso!");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            return lista_alunos;
+        }
+    }
+}
---
+++ CorrecaoDiarioDeClasse/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CorrecaoDiarioDeClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Aluno> lista_alunos = new List<Aluno>();
+
+
+            bool voltar_menu_inicial = false;
+
+            do
+            {
+                Console.Clear();
+
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("SISTEMA DO DIÁRIO DE CLASSE");
+                Console.BackgroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("");
+
+                Console.BackgroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("ALUNOS:");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine("1 - Listar Todos");
+                Console.WriteLine("2 - Cadastrar Aluno");
+                Console.WriteLine("3 - Atualizar Cadastro de Aluno");
+                Console.WriteLine("4 - Remover Aluno");
+
+                Console.BackgroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("FALTAS:");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine("5 - Listar Todos");
+                Console.WriteLine("6 - Cadastrar Aluno");
+                Console.WriteLine("7 - Atualizar Cadastro de Aluno");
+                Console.WriteLine("8 - Remover Aluno");
+
+                Console.BackgroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("NOTAS:");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine("9 - Listar Todos");
+                Console.WriteLine("10 - Cadastrar Aluno");
+                Console.WriteLine("11 - Atualizar Cadastro de Aluno");
+                Console.WriteLine("12 - Remover Aluno");
+
+                int opcao_menu;
+
+                bool opcao_valida_menu = false;
+                do
+                {
+                    opcao_valida_menu = int.TryParse(Console.ReadLine(), out opcao_menu);
+
+                    if(opcao_menu < 1 || opcao_menu > 12 || opcao_valida_menu == false)
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Opção inválida, redigite uma opção:");
+                        Console.BackgroundColor = ConsoleColor.Black;
+
+                        opcao_valida_menu = false;
+                    }
+                } while (!opcao_valida_menu);
+                
+
+                switch(opcao_menu)
+                {
+                    case 1: // Listar Alunos
+                        Aluno.Listar_Alunos(lista_alunos);
+                        break;
+
+                    case 2: // Inserindo um novo Aluno.
+                        lista_alunos = Aluno.Cadastrar(lista_alunos);
+                        Aluno.Listar_Alunos(lista_alunos);
+                        break;
+
+                    case 3: // Atualizar Cadastro de Aluno
+                        Aluno.Listar_Alunos(lista_alunos);
+                        lista_alunos = Aluno.Atualizar(lista_alunos);
+                        break;
+
+                    case 4: // Remover Aluno.
+                        Aluno.Listar_Alunos(lista_alunos);
+                        lista_alunos = Aluno.Excluir(lista_alunos);
+                        break;
+
+                    case 5:
+                        break;
+
+                    case 6:
+                        break;
+
+                    case 7:
+                        break;
+
+                    case 8:
+                        break;
+
+                    case 9:
+                        break;
+
+                    case 10:
+                        break;
+
+                    case 11:
+                        break;
+
+                    case 12:
+                        break;
+                }
+
+                
+
+
+
+
+
+
+
+
+                // Controlando se é para voltar ao menu inicial.
+                Console.WriteLine("Deseja voltar ao menu inicial? S/n");
+                voltar_menu_inicial = (Console.ReadLine().ToUpper() == "S") ? true : false;
+
+            } while (voltar_menu_inicial);
+
+
+            Console.WriteLine("Fim do Programa.");
+            Console.ReadKey();
+        }
+    }
+}
